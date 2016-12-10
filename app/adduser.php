8bb69a19a3ffecb37e32e54e3fd9a5d71fda4cf0
themartto <themartto@gmail.com>
2016-12-10 16:01:59 +0200

    added db connection and adduser function

---
+++ app/adduser.php
@@ -0,0 +1,35 @@
+<?php
+
+include 'config.php';
+
+
+$username = $_POST["username"];
+$email = $_POST["email"];
+$password = $_POST["passwordinput"];
+var_dump($username);
+var_dump($email);
+var_dump($password);
+
+
+/* mysql table structure
+CREATE TABLE Users (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+username VARCHAR(30) NOT NULL,
+email VARCHAR(30) NOT NULL,
+pass VARCHAR(30) NOT NULL
+)   
+*/
+
+
+$sql = "INSERT INTO Users (username, email, pass)
+VALUES ('$username', '$email', '$password')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
+?>
